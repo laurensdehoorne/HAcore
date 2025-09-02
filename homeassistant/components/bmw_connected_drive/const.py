0b346aa99606973d551220f752de303fd88ec9af
laurensdehoorne <55842703+laurensdehoorne@users.noreply.github.com>
2025-09-02 22:12:53 +0200

    Update const.py
    add x_user_agent

--- homeassistant/components/bmw_connected_drive/const.py
+++ homeassistant/components/bmw_connected_drive/const.py
@@ -20,6 +20,8 @@ CONF_CAPTCHA_URL = (
 
 DATA_HASS_CONFIG = "hass_config"
 
+X_USER_AGENT = "android(123bmw);{brand};{app_version};{region}"
+
 UNIT_MAP = {
     "KILOMETERS": UnitOfLength.KILOMETERS,
     "MILES": UnitOfLength.MILES,
